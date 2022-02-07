60f23239249a3e1dad3ab358e9eec2ef835669e1
Redjinator <71242881+Redjinator@users.noreply.github.com>
2022-02-07 09:09:54 -0800

    Refactor:

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import time
 from candle import Candle
 from position import Position
 
+
+timeframe = "1m"
 maxPositions = 3
 positionList = []
 profit  = 0
@@ -11,21 +13,15 @@ wins    = 0
 losses  = 0
 counter = 0
 lastClose = Candle('15m').close
+bar = []
 print('Last 15m close:' + str(lastClose))
 
-
-# In Pinescript, previous values are accessed via an array and I want to emulate that as much as possible here because all of my own algorithms
-# for trading signals were created parsing arrays like this.
-open = []
-close = []
-high = []
-low = []
 
 
 def getClose():
     return Candle('1m').close
 
-#Refactored totalQty
+
 def openPositions():
     openPos = 0
 
@@ -39,7 +35,7 @@ def openPositions():
 def displayData():
     print("---------------------------------------------")
     print('Total Profit: '      + str(profit))
-    print('Total Money Lost: ' + str(moneyLost))
+    print('Total Money Lost: '  + str(moneyLost))
     print('Positions total: '   + str(len(positionList)))
     print('Open Positions: '    + str(len(positionList) - (wins + losses)))
     print('Wins: '              + str(wins))
@@ -50,56 +46,57 @@ while(getClose() < 45000):
 
     counter = counter + 1
 
+    # adds candle information to an array to be accessed later.
+    bar.insert(0, Candle(timeframe))
+
+    close = bar[0]
+
+
     # display current close price
     print('=====================================================')
-    print('Current Close: ' + str(getClose()))
-
-    # adds close information to an array to be accessed later.
-    close.insert(0, getClose())
+    print('Current Close: ' + str(close))
 
 
-    # check total qty of shares from all positions
-    openPositions()
 
-    # if total qty is less than max positions, trade (only valid while 1 qty per position is used)
+    # Check if open positions is less than max positions. (Enables buying)
     if(openPositions() < maxPositions):
 
-        # Buy: held qty = 0
+        # Buy immediately if there are 0 open positions (For testing purposes)
         if(openPositions() == 0):
-            positionList.append(Position(1, getClose()))
-            print('Added a position - ' + str(len(positionList)) + ' target: [' + str(positionList[-1].exitPrice) + '] stop: [' + str(positionList[-1].stopPrice) + ']')
+            positionList.insert(0, Position(1, close))
+            print('Added a position - ' + str(len(positionList)) + ' target: [' + str(positionList[0].exitPrice) + '] stop: [' + str(positionList[0].stopPrice) + ']')
 
 
-        # Buy: additional 1
-        if((getClose()) < ((positionList[-1].entryPrice))):
-            positionList.append(Position(1, getClose()))
-            print('Added a position - ' + str(len(positionList)) + ' target: [' + str(positionList[-1].exitPrice) + '] stop: [' + str(positionList[-1].stopPrice) + ']')
+        # Buy if current close price drops below the previous buy's entry price (For testing purposes)
+        if(close < positionList[-1].entryPrice):
+            positionList.insert(0, Position(1, close))
+            print('Added a position - ' + str(len(positionList)) + ' target: [' + str(positionList[0].exitPrice) + '] stop: [' + str(positionList[0].stopPrice) + ']')
 
 
 
-    # Sell if price closes above an exitPrice
+    # Iterate through
     for pos in positionList:
 
-        c = getClose()
+        if(pos.positionIsOpen):
 
-        # Target hit
-        if ((c > pos.exitPrice) & (pos.positionIsOpen == True)):
-            profit = profit + (c - pos.exitPrice)
-            wins = wins + 1
-            pos.qty     = 0
-            lastClose   = c
-            pos.positionIsOpen = False
+            # Target hit
+            if (close > pos.exitPrice):
+                profit = profit + (close - pos.exitPrice)
+                wins = wins + 1
+                pos.qty     = 0
+                lastClose   = close
+                pos.positionIsOpen = False
 
 
 
-        # Stoploss
-        if ((c < pos.stopPrice) & (pos.positionIsOpen == True)):
-            moneyLost = moneyLost - (c - pos.exitPrice)
-            losses = losses + 1
-            pos.qty     = 0
-            lastClose   = c
-            pos.positionIsOpen = False
-        
+            # Stoploss
+            if (close < pos.stopPrice):
+                moneyLost = moneyLost - (close - pos.exitPrice)
+                losses = losses + 1
+                pos.qty     = 0
+                lastClose   = close
+                pos.positionIsOpen = False
+
 
     displayData()
 
